019320671fcb9a49b2ba5acd0be2b72df4de60a1
Jalil, Ady Syukri <Ady-Syukri.Jalil@external.t-systems.com>
2018-11-29 19:51:24 +0800

    init

---
+++ app.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from time import sleep
+import json
+
+with open('E:\\Data\\CSLMonitoring\\data.json', encoding='utf-8') as s:
+    data = json.loads(s.read())
+
+# urls = []
+
+# again = True
+
+timeInput = input('Enter time in second(s): ')
+
+# while again:
+#     x = input('Enter url:\n')
+#     urls.append(x)
+
+#     insert = input('Insert more urls? (y/n)')
+#     if insert.lower() == 'y':
+#         again = True
+#     elif insert.lower() == 'n':
+#         again = False
+
+# print(urls)
+
+b = webdriver.Firefox('E:\Data\CSLMonitoring')
+# 'E:\Data\IEDrive\IEDriverServer.exe'('C:\chromedrive\geckodriver.exe')
+while True:
+    # for i, url in enumerate(urls):
+    #     b.maximize_window()
+    #     b.get(url)
+    #     sleep(3)
+    for site in data['sites']:
+        b.maximize_window()
+        b.get(site)
+        sleep(int(timeInput))
